f28161c46de4f80a20a1046139750501aed2aa1d
Turupawn <ahmed.hn.43@gmail.com>
2017-04-14 07:08:17 -0600

    0ad added + multiple file expression support

--- check.py
+++ check.py
@@ -4,17 +4,23 @@ import os
 
 from pprint import pprint
 
+def findFilesByExpression(files_expression):
+    files_expression = files_expression.replace("$HOME", os.getenv("HOME"))
+    files_expression = files_expression.replace("$XDG_DATA_HOME", os.getenv("HOME")+"/.local/share")
+    files_expression = files_expression.replace("$XDG_CONFIG_HOME", os.getenv("HOME")+"/.config")
+    files_found = glob.glob(files_expression, recursive=True)
+    for file_found in files_found:
+        pprint(file_found)
+
 def findFiles(file_type):
     pprint(file_type + " files:")
     if file_type in game["files"]:
         files_expression = game["files"][file_type]
-        files_expression = files_expression.replace("$HOME", os.getenv("HOME"))
-        files_expression = files_expression.replace("$XDG_DATA_HOME", os.getenv("HOME")+"/.local/share")
-        files_expression = files_expression.replace("$XDG_CONFIG_HOME", os.getenv("HOME")+"/.config")
-        files_found = glob.glob(files_expression, recursive=True)
-        for file_found in files_found:
-            pprint(file_found)
-    
+        if isinstance(files_expression,str):
+            findFilesByExpression(game["files"][file_type])
+        if isinstance(files_expression,list):
+            for expression in files_expression:
+                findFilesByExpression(expression)
 
 with open('game_list.json') as data_file:    
     data = json.load(data_file)
